1ae3a8226609cee81af39e6d9a24d15f03a156b3
Aymes <aysikh@Aymess-MBP.fios-router.home>
2020-09-01 14:17:25 -0400

    added test runs

--- tools/console.rb
+++ tools/console.rb
@@ -4,5 +4,18 @@ def reload
   load 'config/environment.rb'
 end
 
+
+pels = Bakery.new("Pels")
+
+bun = Dessert.new("Cinnamon Bun")
+buns_ing = Ingredient.new("Cinnamon")
+buns_ing1 = Ingredient.new("Sugar")
+buns_ing2 = Ingredient.new("Dough")
+
+croissant = Dessert.new("Croissant")
+cro_ing = Ingredient.new("Flour")
+cro_ing1 = Ingredient.new("Butter")
+cro_ing2 = Ingredient.new("Sugar")
+
+
 binding.pry
-0
